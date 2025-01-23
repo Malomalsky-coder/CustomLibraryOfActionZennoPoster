b7345f8eb52b14abdc47ed3f1031f0752318612d
Dmitry Malomalsky <crack@ya.ru>
2025-01-24 00:29:31 +0300

    Добавить действие: Выполнить событие

---
+++ Extensions/TabExtension.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ZennoLab.CommandCenter;
+
+namespace CustomLibraryOfActionZennoPoster.Extensions
+{
+    /// <summary>
+    /// Расширяющиме методы для класса <see cref="ZennoLab.CommandCenter.Tab"/>
+    /// </summary>
+    public static class TabExtension
+    {
+        /// <summary>
+        /// Выполнить событие
+        /// </summary>
+        /// <param name="tab"></param>
+        /// <param name="event">Событие</param>
+        /// <param name="xPath">Запрос</param>
+        /// <param name="xPathToPosition">Позиция в дереве элементов</param>
+        /// <param name="millisecondsDelayMin">Подождать перед выполнением, минимально в миллисекундах</param>
+        /// <param name="millisecondsDelayMax">Подождать перед выполнением, максимально в миллисекундах</param>
+        /// <param name="secondWaitingElement">Ждать элемент не более</param>
+        /// <param name="emulationLevel">Уровень эмуляции</param>
+        /// <returns></returns>
+        public static bool ExecuteEvent(
+            this Tab tab,
+            string @event,
+            string xPath,
+            int xPathToPosition = 0,
+            int millisecondsDelayMin = 0,
+            int millisecondsDelayMax = 0,
+            int secondWaitingElement = 0,
+            string emulationLevel = "SuperEmulation")
+        {
+            if (0 > millisecondsDelayMin)
+            {
+                millisecondsDelayMin = 0;
+            }
+
+            if (0 > millisecondsDelayMax)
+            {
+                millisecondsDelayMax = 0;
+            }
+
+            if (0 > secondWaitingElement)
+            {
+                secondWaitingElement = 0;
+            }
+
+            //Подождать перед выполнением
+            Thread.Sleep(new Random().Next(millisecondsDelayMin, millisecondsDelayMax));
+
+            HtmlElement he = tab.FindElementByXPath(xPath, xPathToPosition);
+
+            //Ждать жлемент не более
+            for (int i = 0; i < secondWaitingElement; i++)
+            {
+                if (he.IsVoid) Thread.Sleep(1000);
+                else break;
+            }
+
+            if (he.IsVoid) return false;
+
+            he.RiseEvent(@event, emulationLevel);
+            return true;
+        }
+
+        /// <summary>
+        /// Выполнить событие
+        /// </summary>
+        /// <param name="tab"></param>
+        /// <param name="event">Событие</param>
+        /// <param name="xPath">Запрос</param>
+        /// <param name="xPathToPosition">Позиция в дереве элементов</param>
+        /// <param name="millisecondsDelayMin">Подождать перед выполнением, минимально в миллисекундах</param>
+        /// <param name="millisecondsDelayMax">Подождать перед выполнением, максимально в миллисекундах</param>
+        /// <param name="secondWaitingElement">Ждать элемент не более</param>
+        /// <param name="emulationLevel">Уровень эмуляции</param>
+        /// <returns></returns>
+        public static async Task<bool> ExecuteEventAsync(
+            this Tab tab,
+            string @event,
+            string xPath,
+            int xPathToPosition = 0,
+            int millisecondsDelayMin = 0,
+            int millisecondsDelayMax = 0,
+            int secondWaitingElement = 0,
+            string emulationLevel = "SuperEmulation")
+        {
+            if (0 > millisecondsDelayMin)
+            {
+                millisecondsDelayMin = 0;
+            }
+
+            if (0 > millisecondsDelayMax)
+            {
+                millisecondsDelayMax = 0;
+            }
+
+            if (0 > secondWaitingElement)
+            {
+                secondWaitingElement = 0;
+            }
+
+            //Подождать перед выполнением
+            await Task.Delay(new Random().Next(millisecondsDelayMin, millisecondsDelayMax));
+
+            HtmlElement he = tab.FindElementByXPath(xPath, xPathToPosition);
+
+            //Ждать жлемент не более
+            for (int i = 0; i < secondWaitingElement; i++)
+            {
+                if (he.IsVoid) await Task.Delay(1000);
+                else break;
+            }
+
+            if (he.IsVoid) return await Task.FromResult(false);
+
+            he.RiseEvent(@event, emulationLevel);
+            return await Task.FromResult(true);
+        }
+    }
+}
