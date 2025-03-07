4bc83ebdc2047085b449fb865c568de8bf31829e
Dmitry Malomalsky <crack@ya.ru>
2025-03-07 13:38:50 +0300

    Убрать Async методы, добавить перегрузку WaitElementByXPath

--- Extensions/TabExtension.cs
+++ Extensions/TabExtension.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Threading;
-using System.Threading.Tasks;
 using ZennoLab.CommandCenter;
 
 namespace CustomLibraryOfActionZennoPoster.Extensions
@@ -52,49 +51,6 @@ namespace CustomLibraryOfActionZennoPoster.Extensions
         }
 
         /// <summary>
-        /// Установка значения
-        /// </summary>
-        /// <param name="tab"></param>
-        /// <param name="value">Что</param>
-        /// <param name="attribute">Куда</param>
-        /// <param name="xPath">Запрос</param>
-        /// <param name="xPathToPosition">Позиция в дереве элементов</param>
-        /// <param name="millisecondsDelayMin">Подождать перед выполнением, минимально в миллисекундах</param>
-        /// <param name="millisecondsDelayMax">Подождать перед выполнением, максимально в миллисекундах</param>
-        /// <param name="secondWaitingElement">Ждать элемент не более</param>
-        /// <returns>Ответ, содержащий истину в случае успеха.</returns>
-        /// <example>
-        /// <code>
-        /// Task<bool> result = Task.Run(async () => {
-	    ///     return await instance.ActiveTab.SetValueAsync("0", "value", "//*[@id='text']").ConfigureAwait(false);
-        /// });
-        /// project.SendInfoToLog(result.Result.ToString());
-        /// </code>
-        /// </example>
-        public static async Task<bool> SetValueAsync(
-            this Tab tab,
-            string value,
-            string attribute,
-            string xPath,
-            int xPathToPosition = 0,
-            int millisecondsDelayMin = 0,
-            int millisecondsDelayMax = 0,
-            int secondWaitingElement = 0)
-        {
-            WaitingBeforeExecution(millisecondsDelayMin, millisecondsDelayMax);
-            var waitElResult = await tab.WaitElementByXPathAsync(xPath, xPathToPosition, secondWaitingElement);
-
-            if (waitElResult.Item1)
-            {
-                HtmlElement he = waitElResult.Item2;
-                he.SetAttribute(attribute, value);
-                return await Task.FromResult(true);
-            }
-
-            return await Task.FromResult(false);
-        }
-
-        /// <summary>
         /// Получение значения
         /// </summary>
         /// <param name="tab"></param>
@@ -122,47 +78,6 @@ namespace CustomLibraryOfActionZennoPoster.Extensions
         {
             WaitingBeforeExecution(millisecondsDelayMin, millisecondsDelayMax);
             var waitElResult = tab.WaitElementByXPath(xPath, xPathToPosition, secondWaitingElement);
-
-            if (waitElResult.Item1)
-            {
-                HtmlElement he = waitElResult.Item2;
-                var result = he.GetAttribute(attribute);
-                return result;
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// Получение значения
-        /// </summary>
-        /// <param name="tab"></param>
-        /// <param name="attribute">Что брать</param>
-        /// <param name="xPath">Запрос</param>
-        /// <param name="xPathToPosition">Позиция в дереве элементов</param>
-        /// <param name="millisecondsDelayMin">Подождать перед выполнением, минимально в миллисекундах</param>
-        /// <param name="millisecondsDelayMax">Подождать перед выполнением, максимально в миллисекундах</param>
-        /// <param name="secondWaitingElement">Ждать элемент не более</param>
-        /// <returns>Ответ, содержащий значение атрибута.</returns>
-        /// <example>
-        /// <code>
-        /// Task<string> result = Task.Run(async () => {
-	    /// return await instance.ActiveTab.GetValueAsync("value", "//*[@id='text']").ConfigureAwait(false);
-        /// });
-        /// project.SendInfoToLog(result.Result.ToString());
-        /// </code>
-        /// </example>
-        public static async Task<string> GetValueAsync(
-            this Tab tab,
-            string attribute,
-            string xPath,
-            int xPathToPosition = 0,
-            int millisecondsDelayMin = 0,
-            int millisecondsDelayMax = 0,
-            int secondWaitingElement = 0)
-        {
-            await WaitingBeforeExecutionAsync(millisecondsDelayMin, millisecondsDelayMax);
-            var waitElResult = await tab.WaitElementByXPathAsync(xPath, xPathToPosition, secondWaitingElement);
 
             if (waitElResult.Item1)
             {
@@ -216,55 +131,12 @@ namespace CustomLibraryOfActionZennoPoster.Extensions
         }
 
         /// <summary>
-        /// Выполнить событие
-        /// </summary>
-        /// <param name="tab"></param>
-        /// <param name="event">Событие</param>
-        /// <param name="xPath">Запрос</param>
-        /// <param name="xPathToPosition">Позиция в дереве элементов</param>
-        /// <param name="millisecondsDelayMin">Подождать перед выполнением, минимально в миллисекундах</param>
-        /// <param name="millisecondsDelayMax">Подождать перед выполнением, максимально в миллисекундах</param>
-        /// <param name="secondWaitingElement">Ждать элемент не более</param>
-        /// <param name="emulationLevel">Уровень эмуляции</param>
-        /// <returns>Ответ, содержащий истину в случае успеха.</returns>
-        /// <example>
-        /// <code>
-        /// Task<bool> result = Task.Run(async () => {
-	    ///     return await instance.ActiveTab.ExecuteEventAsync("click", "//div[starts-with(@class,'sign-in-page')]").ConfigureAwait(false);
-        /// });
-        /// project.SendInfoToLog(result.Result.ToString());
-        /// </code>
-        /// </example>
-        public static async Task<bool> ExecuteEventAsync(
-            this Tab tab,
-            string @event,
-            string xPath,
-            int xPathToPosition = 0,
-            int millisecondsDelayMin = 0,
-            int millisecondsDelayMax = 0,
-            int secondWaitingElement = 0,
-            string emulationLevel = "SuperEmulation")
-        {
-            await WaitingBeforeExecutionAsync(millisecondsDelayMin, millisecondsDelayMax);
-            var waitElResult = await tab.WaitElementByXPathAsync(xPath, xPathToPosition, secondWaitingElement);
-
-            if (waitElResult.Item1)
-            {
-                HtmlElement he = waitElResult.Item2;
-                he.RiseEvent(@event, emulationLevel);
-                return await Task.FromResult(true);
-            }
-
-            return await Task.FromResult(false);
-        }
-
-        /// <summary>
         /// Дождаться появления элемента не более (сек)
         /// </summary>
         /// <param name="tab"></param>
-        /// <param name="xPath"></param>
-        /// <param name="xPathToPosition"></param>
-        /// <param name="secondWaitingElement"></param>
+        /// <param name="xPath">xPath выражение</param>
+        /// <param name="xPathToPosition">Позиция элемента</param>
+        /// <param name="secondWaitingElement">Ждать элемент не более (сек)</param>
         /// <returns></returns>
         public static (bool, HtmlElement) WaitElementByXPath(this Tab tab, string xPath, int xPathToPosition = 0, int secondWaitingElement = 0)
         {
@@ -292,12 +164,23 @@ namespace CustomLibraryOfActionZennoPoster.Extensions
         /// Дождаться появления элемента не более (сек)
         /// </summary>
         /// <param name="tab"></param>
-        /// <param name="xPath"></param>
-        /// <param name="xPathToPosition"></param>
-        /// <param name="secondWaitingElement"></param>
+        /// <param name="xPath">xPath выражение</param>
+        /// <param name="xPathToPosition">Позиция элемента</param>
+        /// <param name="waitBeforeExecuting">Подождать перед выполнением</param>
+        /// <param name="secondWaitingElement">Ждать элемент не более (сек)</param>
         /// <returns></returns>
-        public static async Task<(bool, HtmlElement)> WaitElementByXPathAsync(this Tab tab, string xPath, int xPathToPosition = 0, int secondWaitingElement = 0)
+        public static (bool, HtmlElement) WaitElementByXPath(this Tab tab, string xPath, int xPathToPosition = 0, int waitBeforeExecuting = 0, int secondWaitingElement = 0)
         {
+            if (0 > waitBeforeExecuting)
+            {
+                waitBeforeExecuting = 0;
+            }
+
+            if (waitBeforeExecuting > 0)
+            {
+                Thread.Sleep(waitBeforeExecuting * 1000);
+            }
+
             if (0 > secondWaitingElement)
             {
                 secondWaitingElement = 0;
@@ -310,7 +193,7 @@ namespace CustomLibraryOfActionZennoPoster.Extensions
             {
                 he = tab.FindElementByXPath(xPath, xPathToPosition);
 
-                if (he.IsVoid) await Task.Delay(1000);
+                if (he.IsVoid) Thread.Sleep(1000);
                 else break;
             }
 
@@ -321,8 +204,8 @@ namespace CustomLibraryOfActionZennoPoster.Extensions
         /// <summary>
         /// Подождать перед выполнением
         /// </summary>
-        /// <param name="millisecondsDelayMin"></param>
-        /// <param name="millisecondsDelayMax"></param>
+        /// <param name="millisecondsDelayMin">От</param>
+        /// <param name="millisecondsDelayMax">До</param>
         private static void WaitingBeforeExecution(int millisecondsDelayMin, int millisecondsDelayMax)
         {
             if (0 > millisecondsDelayMin)
@@ -336,27 +219,6 @@ namespace CustomLibraryOfActionZennoPoster.Extensions
             }
 
             Thread.Sleep(new Random().Next(millisecondsDelayMin, millisecondsDelayMax));
-        }
-
-        /// <summary>
-        /// Подождать перед выполнением
-        /// </summary>
-        /// <param name="millisecondsDelayMin"></param>
-        /// <param name="millisecondsDelayMax"></param>
-        /// <returns></returns>
-        private static async Task WaitingBeforeExecutionAsync(int millisecondsDelayMin, int millisecondsDelayMax)
-        {
-            if (0 > millisecondsDelayMin)
-            {
-                millisecondsDelayMin = 0;
-            }
-
-            if (0 > millisecondsDelayMax)
-            {
-                millisecondsDelayMax = 0;
-            }
-
-            await Task.Delay(new Random().Next(millisecondsDelayMin, millisecondsDelayMax));
         }
     }
 }
